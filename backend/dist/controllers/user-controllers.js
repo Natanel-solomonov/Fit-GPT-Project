d43b59608db178d837c4ee42581e29553ae995c0
Natanel-solomonov <143336407+Natanel-solomonov@users.noreply.github.com>
2024-07-17 21:59:20 -0400

    Commit 07/17/24 9:59 pm

--- backend/dist/controllers/user-controllers.js
+++ backend/dist/controllers/user-controllers.js
@@ -1,9 +1,12 @@
 import User from "../models/User.js";
-export const getAllUsers = async () => {
+export const getAllUsers = async (req, res, next) => {
     try {
         const users = await User.find();
+        return res.status(200).json({ message: "OK", users });
     }
     catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "ERROR", cause: error });
     }
 };
 //# sourceMappingURL=user-controllers.js.map
