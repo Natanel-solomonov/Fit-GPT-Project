ae85f20257253c8b9352dd78c3c919720857f393
Natanel-solomonov <143336407+Natanel-solomonov@users.noreply.github.com>
2024-07-20 22:15:34 -0400

    Final Commit for 07/20

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,18 +1,27 @@
 import { Box } from '@mui/material';
-import React from 'react'
+import React from 'react';
 
-const Login =() =>{
-    return(
-    <Box width = {"100%"} height={"100%"} display="flex" flex={1}>
-        <Box 
-        padding = {8} 
-        mt={8} 
-        display= {{md:"flex",sm:"none",xs:"none"}}
-        >
-        <img src= "FitGPTLogo.png"alt="openai" style={{width:"400px"}}/>
+const Login = () => {
+    return (
+        <Box width={"100%"} height={"100vh"} display="flex" flexDirection="column" justifyContent="space-between">
+            <Box 
+                display={{ md: "flex", sm: "none", xs: "none" }} 
+                padding={2}
+                alignSelf="flex-start"
+                style={{ marginBottom: "16px" }}
+            >
+                <img src="/FitGPTLogo.png" alt="openai" style={{ width: "400px", marginLeft: "-90px", marginTop: "175px" }} />
+            </Box>
+            <Box 
+                display={"flex"} 
+                flex={{ xs: 1, md: 0.5 }} 
+                justifyContent={"center"} 
+                alignItems={"center"}
+                padding={2}
+                ml={'Auto'}
+                mt={16}
+            ></Box>
         </Box>
-        <Box></Box>
-    </Box>
     );
 }
 
