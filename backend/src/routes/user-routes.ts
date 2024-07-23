baf5b27085be8a53287e408c8177acde781b297e
Natanel-solomonov <143336407+Natanel-solomonov@users.noreply.github.com>
2024-07-23 15:20:39 -0400

    Revert Commit for 07/23

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,3 +1,4 @@
+
 import {Router} from 'express';
 import { getAllUsers, userLogin, userSignup } from '../controllers/user-controllers.js';
 import{ loginValidator, signupValidator, validate } from "../utils/validators.js";
@@ -9,4 +10,3 @@ userRoutes.post("/signup",validate(signupValidator),userSignup);
 userRoutes.post("/login",validate(loginValidator),userLogin);
 
 export default userRoutes;
-
