420a363b619b8a11de92c96f11eaa8a74283db8b
Natanel-solomonov <143336407+Natanel-solomonov@users.noreply.github.com>
2024-07-23 17:09:09 -0400

    User Verification Commit

--- frontend/src/helpers/api-communicator.ts
+++ frontend/src/helpers/api-communicator.ts
@@ -9,3 +9,13 @@ export const loginUser= async (email:string,password:string) => {
     return data;
 
 };
+
+export const checkAuthStatus= async () => {
+    const res = await axios.get("/user/auth-status");
+    if(res.status!==201&&res.status!==200){
+        throw new Error("Unable to Authenticate");
+    }
+    const data= await res.data;
+    return data;
+
+};
