a620b4b8c6c01c45d7bbc2e6af65e8a73a6f6370
Natanel-solomonov <143336407+Natanel-solomonov@users.noreply.github.com>
2024-07-25 16:27:35 -0400

    Signup Page Commit

--- frontend/src/helpers/api-communicator.ts
+++ frontend/src/helpers/api-communicator.ts
@@ -10,6 +10,16 @@ export const loginUser= async (email:string,password:string) => {
 
 };
 
+export const signupUser= async (name:string, email:string,password:string) => {
+    const res = await axios.post("/user/signup", {name, email,password});
+    if(res.status!==201&&res.status!==200){
+        throw new Error("Unable to Signup");
+    }
+    const data= await res.data;
+    return data;
+
+};
+
 export const checkAuthStatus= async () => {
     const res = await axios.get("/user/auth-status");
     if(res.status!==201&&res.status!==200){
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,9 +1,137 @@
-import React from 'react'
+import { Box, Button, Typography } from '@mui/material';
+import React, { useEffect } from 'react';
+import { IoIosLogIn } from 'react-icons/io';
+import CustomizedInput from '../components/shared/CustomizedInput';
+import {toast } from 'react-hot-toast'
+import { useAuth } from '../context/AuthContext';
+import {  useNavigate } from 'react-router-dom';
 
-const Signup =() =>{
-    return(
-        <div>Signup</div>
-    )
-}
+
+const Signup = () => {
+    const navigate = useNavigate();
+    const auth = useAuth()
+    const handleSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const email = formData.get("email") as string;
+        const name = formData.get("name") as string
+        const password = formData.get("password") as string;
+        console.log(email,password);
+        try{
+            toast.loading("Signing Up", {id:"signup"});
+            await auth?.signup(name,email,password);
+            toast.success("Signed In Successfully", {id:"signup"});
+        }catch(error){
+            console.log(error);
+            toast.error("Signing Up Failed", {id: "signup"})
+        }
+        
+    };
+    useEffect(() => {
+        if (auth?.user) {
+          return navigate("/chat");
+        }
+      }, [auth]);
+   
+    return (
+        <Box width={'100%'} height={'100%'} display="flex" flexDirection={{ xs: "column", md: "row" }} justifyContent="center" alignItems="flex-end">
+            <Box
+                padding={8}
+                display={{ md: "flex", sm: "none", xs: "none" }}
+                sx={{
+                    position: "relative",
+                    left: "-300px",
+                    bottom: "-150px"
+                }}
+            >
+                <img
+                    src="FitGPTLogo.png"
+                    alt="FitGPTLogo.png"
+                    style={{
+                        width: "620px",
+                    }}
+                />
+            </Box>
+            <Box
+                display={'flex'}
+                flex={{ xs: 1, md: 0.5 }}
+                justifyContent={'center'}
+                alignItems={'flex-end'}
+                padding={2}
+                mb={4}
+                sx={{
+                    position: "relative",
+                    left: "-410px"
+                }}
+            >
+                <form
+                    onSubmit={handleSubmit}
+                    style={{
+                        margin: 'auto',
+                        padding: '30px',
+                        boxShadow: "0 0 20px rgba(255,255,255,1)",
+                        borderRadius: '10px',
+                        border: "none",
+                    }}
+                >
+                    <Box sx={{
+                        display: 'flex',
+                        flexDirection: "column",
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}>
+                        <Typography
+                            variant='h4'
+                            textAlign="center"
+                            padding={2}
+                            fontWeight={600}
+                        >
+                            Signup - Your Fitness Journey Starts Here
+                        </Typography>
+                        <CustomizedInput type="text" name="name" label="Name" />
+                        <CustomizedInput type="email" name="email" label="Email" />
+                        <CustomizedInput type="password" name="password" label="Password" />
+                        <Button
+                            type="submit"
+                            sx={{
+                                px: 2,
+                                py: 1,
+                                mt: 2,
+                                width: "400px",
+                                borderRadius: 2,
+                                bgcolor: "gold",
+                                color: "black",
+                                ":hover": {
+                                    bgcolor: 'white',
+                                    color: 'black',
+                                }
+                            }}
+                            endIcon={<IoIosLogIn />}
+                        >
+                            Signup
+                        </Button>
+                    </Box>
+                </form>
+            </Box>
+            <Box
+                padding={8}
+                display={{ md: "flex", sm: "none", xs: "none" }}
+                sx={{
+                    position: "absolute",
+                    right: "-115px",
+                    bottom: "-150px"
+                }}
+            >
+                <img
+                    src="FitGPTLogoFemale.png"
+                    alt="FitGPTLogoFemale.png"
+                    style={{
+                        width: "600px",
+                    }}
+                />
+            </Box>
+        </Box>
+    );
+};
 
 export default Signup;
