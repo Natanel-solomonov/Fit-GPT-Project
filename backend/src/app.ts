34b952c11b6009e157dc2c5f709474cb226c7278
Natanel-solomonov <143336407+Natanel-solomonov@users.noreply.github.com>
2024-08-04 12:40:52 -0400

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,33 +3,18 @@ import { config } from 'dotenv';
 import morgan from 'morgan';
 import appRouter from './routes/index.js';
 import cookieParser from 'cookie-parser';
-import cors from 'cors';
+const cors = require("cors")
 
 config();
 const app = express();
 
-const allowedOrigins = [
-  "https://fitgptfrontend.onrender.com",
-  "http://localhost:5173"
-];
+// Configure CORS options
 
-const corsOptions = {
-  origin: (origin: string, callback: (err: Error | null, allow?: boolean) => void) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS", "CONNECT", "TRACE"],
-  allowedHeaders: ["Content-Type", "Authorization", "X-Content-Type-Options", "Accept", "X-Requested-With", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"],
-  credentials: true,
-  maxAge: 7200
-};
-
-// Use CORS middleware
-app.use(cors(corsOptions));
-
+app.use(
+  cors({
+    orgin: "https://fitgptfrontend.onrender.com"
+  })
+)
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
