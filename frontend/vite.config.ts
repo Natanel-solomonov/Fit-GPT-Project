fb4070194ee4fccda228d8241f2025c0c41e93a1
Natanel-solomonov <143336407+Natanel-solomonov@users.noreply.github.com>
2024-08-04 19:20:20 -0400

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server: {
-  //   proxy: {
-  //     '/api/v1': {
-  //       target: 'https://fit-gpt-backend.onrender.com',
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api\/v1/, '')
-  //     }
-  //   }
-  // },
+  server: {
+    proxy: {
+      '/api/v1': {
+        target: 'https://fit-gpt-backend.onrender.com',
+        changeOrigin: true
+      
+      }
+    }
+  },
   plugins: [react()],
 })
