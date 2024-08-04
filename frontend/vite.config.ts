ac6f052b88d5e622c1307b9abe36d52d52f8df06
Natanel-solomonov <143336407+Natanel-solomonov@users.noreply.github.com>
2024-08-04 13:14:31 -0400

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api/v1': {
+        target: 'https://mern-fit-gpt-backend.onrender.com',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api\/v1/, '')
+      },
+    },
+  },
   plugins: [react()],
-})
+});
