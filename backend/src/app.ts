26e95b79dc076d8b07423fbb69fc92ab5d61cc02
Natanel-solomonov <143336407+Natanel-solomonov@users.noreply.github.com>
2024-08-04 18:36:52 -0400

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,20 +1,30 @@
-import express from 'express'
+import express from 'express';
 import { config } from 'dotenv';
-import morgan from 'morgan'
+import morgan from 'morgan';
 import appRouter from './routes/index.js';
 import cookieParser from 'cookie-parser';
-import cors from 'cors'
+const cors = require("cors")
 
 config();
 const app = express();
 
+// Configure CORS options
 
-//middlewares
-app.use(cors({origin: "http://localhost:5173",credentials:true}))
+app.use(
+  cors({
+    orgin: "https://fit-gpt-frontend.onrender.com"
+  })
+)
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
-//remove it in production
-app.use(morgan("dev")) ;
 
-app.use("/api/v1", appRouter);
+// Remove it in production
+app.use(morgan('dev'));
+
+app.use('/api/v1', appRouter);
+
+app.get('/test-cors', (req, res) => {
+  res.json({ message: 'CORS configuration is working' });
+});
+
 export default app;
