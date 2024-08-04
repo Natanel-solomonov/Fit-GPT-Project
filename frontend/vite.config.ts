4f4f37b9fbe8f8b54cd61029d5269e2dd008e60a
Natanel-solomonov <143336407+Natanel-solomonov@users.noreply.github.com>
2024-08-04 18:21:03 -0400

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    proxy:{
+      '/ap1/v1': 'https://fit-gpt-backend.onrender.com'
+    }
+  },
   plugins: [react()],
 })
