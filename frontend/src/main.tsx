3b60a464811c783789f9dade7f1ce7aff77ccd0d
Natanel-solomonov <143336407+Natanel-solomonov@users.noreply.github.com>
2024-08-04 20:28:32 -0400

    refresh pull

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,11 +7,7 @@ import {BrowserRouter} from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.tsx'
 import{Toaster} from 'react-hot-toast'
 import axios from "axios"
-<<<<<<< HEAD
 axios.defaults.baseURL = '/api/v1';
-=======
-axios.defaults.baseURL = 'https://fit-gpt-backend.onrender.com';
->>>>>>> ed915ec91b55762e9726fdb21bd7f5d6bdbbf3b4
 axios.defaults.withCredentials= true;
 
 const theme = createTheme({
