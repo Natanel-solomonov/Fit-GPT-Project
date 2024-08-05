4fbdd71c083eb6df1d653c8e13250cc26e49477d
Natanel-solomonov <143336407+Natanel-solomonov@users.noreply.github.com>
2024-08-04 20:32:29 -0400

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,27 +3,19 @@ import { config } from 'dotenv';
 import morgan from 'morgan';
 import appRouter from './routes/index.js';
 import cookieParser from 'cookie-parser';
-const cors = require("cors")
-<<<<<<< HEAD
-import bodyParser from 'body-parser'
-=======
+import cors from 'cors';
 import bodyParser from 'body-parser';
 
->>>>>>> ed915ec91b55762e9726fdb21bd7f5d6bdbbf3b4
 config();
 const app = express();
 
 // Configure CORS options
-
 app.use(
   cors({
-<<<<<<< HEAD
-    orgin: "https://fit-gpt-frontend.onrender.com"
-=======
-    origin: "https://fit-gpt-frontend.onrender.com"
->>>>>>> ed915ec91b55762e9726fdb21bd7f5d6bdbbf3b4
+    origin: "https://fit-gpt-frontend.onrender.com" // Fixed typo: "orgin" to "origin"
   })
-)
+);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
